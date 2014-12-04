c8fa72162536570871ff4159ad08ff16b4df6061
Joshua Anderson <joshua.anderson688@gmail.com>
2014-12-04 00:21:26 +0000

    Updated CSS, added contact/testimonials
    Signed-off-by: Joshua Anderson <joshua.anderson688@gmail.com>

--- static-assets/js/app/main.js
+++ static-assets/js/app/main.js
@@ -4,7 +4,19 @@ define(['./selector', './preloader', './events', './pages', './nav'], function(S
 	var W = Selector.w;
 	var Main = Main || {};
 	var Scroll = new Nav(30, null);
+	var _clearOverlay = function _clearOverlay(overlay) {
+		overlay.style.display = 'block';
+		overlay.className = '';
+	};
+	var _addOverlayClass = function _addOverlayClass(overlay) {
+		overlay.className = 'hidden';
+	};
+	var _removeOverlayStyle = function _setOverlayStyle(overlay) {
+		overlay.style.display = 'none';
+	};
+
 	Events.addToWindow('resize', Pages.setAllHeightsWidths);
+
 	var menuItems = S(".menu-item");
 
 	if(menuItems) {
@@ -18,21 +30,25 @@ define(['./selector', './preloader', './events', './pages', './nav'], function(S
 			});
 		}
 	}
-	var overlays = S("*[data-overlay]", true);
+	var overlays = S('*[data-overlay]', true);
 	var overlayBg = S('#overlay-background');
-	Events.addToElement(overlayBg, 'click', function() {
-		overlayBg.className = 'hidden';
+	var overlayContact = S('#overlay-contact');
+	var overlayClose = S('#overlay-contact a.close', true);
+	Events.addToElements([overlayBg, overlayClose], 'click', function() {
+		_addOverlayClass(overlayBg);
+		_addOverlayClass(overlayContact);
 		W.setTimeout(function() {
-			overlayBg.style.display = 'none';
+			_removeOverlayStyle(overlayContact);
+			_removeOverlayStyle(overlayBg);
 		}, 1000);
-	})
+	});
 	if(overlays) {
 		for(var i = 0; i < overlays.length; i++) {
 			var overlay = overlays[i];
 			Events.addToElement(overlay, 'click', function() {
 				var overlayId = this.dataset.overlay;
-				overlayBg.style.display = 'block';
-				overlayBg.className = "";
+				_clearOverlay(S('#overlay-' + overlayId));
+				_clearOverlay(overlayBg);
 			});
 		}
 	}
