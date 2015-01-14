2bb0312d80e4a22c15d7b14e135e243a4fd8f473
Joshua Anderson <joshua.anderson@gamesys.co.uk>
2015-01-14 19:23:22 +0000

    Added some grunt tasks

---
+++ development/js/app/selector.js
@@ -0,0 +1,46 @@
+define(function () {
+    'use strict';
+    var D = document;
+    var W = window;
+    var S = function S(obj, querySelector) {
+        querySelector = querySelector || false;
+        if (querySelector) {
+            var objects = D.querySelectorAll(obj);
+            if (objects.length === 1) {
+                return D.querySelector(obj);
+            } else {
+                return objects;
+            }
+        }
+        var objSub = obj.substr(1, obj.length);
+        var _t = function _t(tag) {
+            return D.getElementsByTagName(tag);
+        };
+
+        var _i = function _i(id) {
+            return D.getElementById(id);
+        };
+
+        var _c = function _c(className) {
+            var allObjs = D.getElementsByClassName(className);
+            if (allObjs.length === 0) {
+                return null;
+            } else {
+                return allObjs;
+            }
+        };
+        if (obj.indexOf('.') === 0) {
+            return _c(objSub);
+        } else if (obj.indexOf('#') === 0) {
+            return _i(objSub);
+        } else {
+            return _t(obj);
+        }
+    };
+
+    return {
+        'd': D,
+        'selector': S,
+        'w': W
+    };
+});
