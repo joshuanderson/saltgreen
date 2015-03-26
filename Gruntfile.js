844c3de6dd52e23466c159a63a4ef2b1f844d737
Joshua Anderson <joshua.anderson688@gmail.com>
2015-03-26 22:19:49 +0000

    Updated grunt file and tasks
    Signed-off-by: Joshua Anderson <joshua.anderson688@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,70 +1,33 @@
 (function () {
 	'use strict';
+
 	module.exports = function (grunt) {
-		var rjsConfig = {
-			appDir: 'development',
-			baseUrl: './',
-			dir: 'static-assets',
-			optimize: 'uglify2',
-			optimizeCss: 'none',
-			preserveLicenseComments: false,
-			skipDirOptimize: false,
-		};
 		var initialConfig = {
 			pkg: grunt.file.readJSON('package.json'),
-
 			requirejs: {
 				compile: {
-					options: rjsConfig
+					options: {
+						appDir: 'development/js',
+						baseUrl: './',
+						dir: 'static-assets/js',
+						optimize: 'uglify2',
+						preserveLicenseComments: false,
+						skipDirOptimize: false,
+					}
 				}
 			},
-
-			copy: {
-				manifestFiles: {
-					expand: true,
-					cwd: 'development',
-					src: [
-						'require.json',
-						'component.json'
-					],
-					dest: 'static-assets'
-				}
-			},
-
 			compass: {
 				options: {
 					basePath: '',
-					outputStyle: 'expanded',
+					force: true,
+					noLineComments: true,
+					outputStyle: 'compressed',
 					trace: true,
 					sassDir: 'development/css',
 					cssDir: 'static-assets/css',
 					specify: 'development/css/*.scss'
 				},
-				dev: {},
-				dist: {
-					options: {
-						trace: false
-					}
-				}
-			},
-			concat: {
-				options: {
-					separator: ';'
-				},
-				dist: {
-					src: [ 'development/js/**/*.js' ],
-					dest: 'static-assets/js/'
-				}
-			},
-			uglify: {
-				options: {
-					banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-				},
-				dist: {
-					files: {
-						'static-assets/js/<%= pkg.name %>.min.js': [ '<%= concat.dist.dest %>' ]
-					}
-				}
+				dev: {}
 			},
 			jshint: {
 				files: [ 'Gruntfile.js', 'development/js/**/*.js' ],
@@ -78,18 +41,17 @@
 			},
 			watch: {
 				files: [ '<%= jshint.files %>' ],
-				tasks: [ 'jshint' ]
+				tasks: [ 'jshint', 'compass', 'requirejs' ]
 			}
 		};
+
 		grunt.initConfig(initialConfig);
 		grunt.loadNpmTasks('grunt-contrib-requirejs');
-		grunt.loadNpmTasks('grunt-contrib-sass');
+		// grunt.loadNpmTasks('grunt-browserify');
 		grunt.loadNpmTasks('grunt-contrib-compass');
-		grunt.loadNpmTasks('grunt-contrib-uglify');
 		grunt.loadNpmTasks('grunt-contrib-jshint');
 		grunt.loadNpmTasks('grunt-contrib-watch');
-		grunt.loadNpmTasks('grunt-contrib-concat');
 
-		grunt.registerTask('default', [ 'jshint', 'compass', 'requirejs' ]);
+		grunt.registerTask('default', [ 'jshint', 'compass', 'requirejs', 'watch' ]);
 	};
 })();
